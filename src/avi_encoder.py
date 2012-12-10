6ff6b378075283d9149aae5b822425a1b6191ec0
steven <ramsessk@gmail.com>
2012-12-10 22:04:12 +0800

    add class for one srt conversion

--- src/avi_encoder.py
+++ src/avi_encoder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys
-
+import logging
 
 
 '''
@@ -12,70 +12,177 @@ import os, sys
 3. preprocess to encoder [[mp4, srt_1, srt2], [mp4,srt], [mp4,srt_1, srt_2, srt_3],...]
 4. avi to mp4 (external tool : HandbreakCLI)
 
+~/Movies/HandBreakCLI/HandBrakeCLI -i $1.avi -o $1.mp4 --srt-codeset UTF-8 --srt-file $var1,$var2 
+		--srt-lang kor,eng --srt-default 1 --subtitle-burn 1
+
 '''
 
+###################################################################################################
+def searchFiles(FileExtension, directory):
+	""" Looking for files which has 'FileExtension' in current directory.
 
+	:param FileExtension: file extension to look for, Ex. avi, or mkv. it should not include '.'.
+	:type FileExtension : str
+	:param directory: directory to look for
+	:type directory : str
 
-#os.system('cd /Volumes/K9/movies/Numbers/S4')
-if len(sys.argv) <= 1:
-	print """
-	usage : python mp2encode.py avi_directory
-	example : avi_dirctory : /Volumes/usb_stick/movies/
-	
-	Location of related files to execution .....
-	HandbreakCLI location : ~/Movies/HandBreakCLI/
-	avi2mp4withsrt.sh or avi2mp4.sh location : ~/bin/
-	english_from_srt.py location : ~/bin/
+	:returns: Returns a list of files found:
+		:rtype: list
 	"""
-	exit()
-
-avi_files = []
-srt_files = []
-dirs = os.listdir(sys.argv[1])
-for s in dirs:
-	#print s
-	if s[-4:] == '.avi':
-		avi_files.append(s[0:-4])
-		#print s[0:-4]
-
-for s in dirs:
-	#print s
-	if s[-4:] == '.avi':
-		srt_files.append(s[0:-4])
-		#print s[0:-4]
-
-print len(avi_files), len(srt_files)
-
-print "extract english subtile only from srt file ..."
-print
-curdir = os.getcwd()
-#print curdir
-os.system('cd ' + sys.argv[1])
-os.system('python ~/bin/english_from_srt.py')
-os.system('cd ' + curdir)
-print "completed english subtile extraction ..."
-os.getcwd()
-
-n = 0
-for s in avi_files:
-	print "==============================================================="
-	print 'start : ', s
-	print "==============================================================="
 	
-	no_srt = False
-	try :
-		idx = srt_files.index(avi_files[n])
-	except:
-		print avi_files[n], ': No SRT file found'
-		no_srt = True
+	foundfiles = []
+	dir_list = os.listdir(directory)
+	#print FileExtension, dirs
+	FileExtension = '.' + FileExtension
+	for s in dir_list:
+		if len(s) > len(FileExtension) and s[-len(FileExtension):] == FileExtension:
+			foundfiles.append(s)
+			logging.info("Found: {0}".format(s))
+			#print s
+
+
+	return foundfiles
+
+###################################################################################################
+def matchFiles(files1, ext, files2):
+	""" match list index which has same file name except extension.
+	Ex. for return data : [[mp4, srt_1, srt2], [mp4,srt], [mp4,srt_1, srt_2, srt_3],...]
+
+	:param files1: It should be list of movie files
+	:type files : list
+	:param ext: movie file extension
+	:type ext : str
+	:param files2: list of srt files
+	:type files2 : list
+
+	:returns: Returns a list of matched files:
+		:rtype: list
+	"""
+	
+	matched_list = []
+	job = []
+	for s1 in files1:
+		job.append(s1)
+		name = s1[0:-len(ext)-1]
+		print name
+		for s2 in files2:
+			if (name == s2[0:len(name)]):
+				job.append(s2)
+		matched_list.append(job)
+		job = []
+	return matched_list
+
+
+###################################################################################################
+
+logging.basicConfig(level=logging.ERROR)
+if( os.path.exists(os.path.expanduser('~/bin/HandBrakeCLI')) == False ):
+	print 'External encoder not available. It should be located at /Users/[USER]/bin'
+	print 'External encoder : HandBrakeCLI '
+	sys.exit()
+
+avis = searchFiles('avi', sys.argv[1])
+srts = searchFiles('srt', sys.argv[1])
+
+m = matchFiles(avis, 'avi', srts)
+print m
+#print avis, srts
+
+for m1 in m:
+	#print m1
+	cmd = '~/bin/HandBrakeCLI -i '
+	cmd = cmd + m1[0]
+	#print (m1[0])[0:-4]
+	cmd = cmd + ' -o ' + (m1[0])[0:-4] + '.mp4' + ' --srt-codeset UTF-8 '
+	srt_num = len(m1) - 1
+	#print 'srt_num=', srt_num
+	if( srt_num > 0 ):
+		cmd = cmd + ' --srt-file '
+		i = 1
+		while srt_num > 0:
+			cmd = cmd + m1[i]
+			srt_num = srt_num - 1
+			if( srt_num > 0):
+				cmd = cmd + ','
+			i = i + 1
 		
-	if no_srt:
-		cmd = './avi2mp4.sh ' + sys.argv[1] + avi_files[n]
-	else:
-		cmd = './avi2mp4withsrt.sh ' + sys.argv[1] + avi_files[n]
-	
+		if((len(m1)-1) > 1):
+			cmd = cmd + ' --srt-lang kor, eng '
+			cmd = cmd + ' --srt-default 1 --subtitle-burn 1'
+			
+		cmd = cmd + ' --srt-default 1 --subtitle-burn 1'
+	else:	# no subtitle
+		pass
+	cmd = cmd + ' --preset="AppleTV 2" '
 	print cmd
-	os.system(cmd)
-	n = n + 1
+	#os.system(cmd)
+
+
+#~/Movies/HandBreakCLI/HandBrakeCLI -i $1.avi -o $1.mp4 --srt-codeset UTF-8 --srt-file $var1,$var2 
+#		--srt-lang kor,eng --srt-default 1 --subtitle-burn 1
+
+
+
+
+#if len(sys.argv) <= 1:
+#	print """
+#	usage : python mp2encode.py avi_directory
+#	example : avi_dirctory : /Volumes/usb_stick/movies/
+#	
+#	Location of related files to execution .....
+#	HandbreakCLI location : ~/Movies/HandBreakCLI/
+#	avi2mp4withsrt.sh or avi2mp4.sh location : ~/bin/
+#	english_from_srt.py location : ~/bin/
+#	"""
+#	exit()
+#
+#avi_files = []
+#srt_files = []
+#dirs = os.listdir(sys.argv[1])
+#for s in dirs:
+#	#print s
+#	if s[-4:] == '.avi':
+#		avi_files.append(s[0:-4])
+#		#print s[0:-4]
+#
+#for s in dirs:
+#	#print s
+#	if s[-4:] == '.avi':
+#		srt_files.append(s[0:-4])
+#		#print s[0:-4]
+#
+#print len(avi_files), len(srt_files)
+#
+#print "extract english subtile only from srt file ..."
+#print
+#curdir = os.getcwd()
+##print curdir
+#os.system('cd ' + sys.argv[1])
+#os.system('python ~/bin/english_from_srt.py')
+#os.system('cd ' + curdir)
+#print "completed english subtile extraction ..."
+#os.getcwd()
+#
+#n = 0
+#for s in avi_files:
+#	print "==============================================================="
+#	print 'start : ', s
+#	print "==============================================================="
+#	
+#	no_srt = False
+#	try :
+#		idx = srt_files.index(avi_files[n])
+#	except:
+#		print avi_files[n], ': No SRT file found'
+#		no_srt = True
+#		
+#	if no_srt:
+#		cmd = './avi2mp4.sh ' + sys.argv[1] + avi_files[n]
+#	else:
+#		cmd = './avi2mp4withsrt.sh ' + sys.argv[1] + avi_files[n]
+#	
+#	print cmd
+#	os.system(cmd)
+#	n = n + 1
 
 #print srt_files.index('abc')
--- src/srts_2_srt.py
+++ src/srts_2_srt.py
@@ -18,9 +18,172 @@ By steven <ramsessk@gmail.com>
 
 #import time
 import os, sys
-from operator import itemgetter, attrgetter
+import logging
+
+class SeparateSrtFiles(object):
+	#--------------------------------------------------------------------------------------------------
+	def __init__(self, fname="", enc='UTF-8'):
+		self.srt_fname = fname
+		self.encode = enc
+
+	#--------------------------------------------------------------------------------------------------
+	# Find out whether this subtitles is multiple language subtitles.
+	def isMultipleLanguageSubtitle(self, subtitles ):
+		prev_stime = 0
+		eng_title_started = 0
+		for title in subtitles:
+			#print title
+			tstamp_str = title[1]
+			#print tstamp_str
+			tstamp_str = tstamp_str.replace(':',' ')
+			tstamp_str = tstamp_str.replace(',', ' ')
+			tstamp_str = tstamp_str.replace(' --> ', ' ')
+			tstamp_str = tstamp_str.split(' ')
+			tstamp = []
+			for s in tstamp_str:
+				tstamp.append(int(s))
+			stime = tstamp[0]*24 + tstamp[1]*60 + tstamp[2]
+			#print stime, tstamp
+			if( stime < prev_stime ) :
+				break
+			prev_stime = stime
+			eng_title_started = eng_title_started + 1
+		if eng_title_started == len(subtitles):
+			eng_title_started = 1;
+			return False
+		return True;
+
+	#--------------------------------------------------------------------------------------------------
+	#	subtitles = [ ['1', '00:00 --> 00:00', 'abcdef'], ['2', '00:01 --> 00:01', 'erqwer'], ....
+	#	subtitles[0] = ['1', '00:00 --> 00:00', 'abcdef']
+	#
+	def writeNewSrtFiles(self, subtitles):
+		if self.isMultipleLanguageSubtitle(subtitles) == False:
+			logging.info("Nothing to do: {0}".format(self.srt_fname))
+			return
+		
+		num_of_lang = 1
+		wfname = self.srt_fname[0:-4] + '_' + str(num_of_lang) + '.srt'
+		logging.info("writing : {0}".format(wfname))
+		f = open(wfname, 'w')
+		n = 1
+		line_no = 1
+		prev_stime = 0
+		for title in subtitles:
+			#	title = ['1', '00:00 --> 00:00', 'abcdef']
+			#print title
+			tstamp_str = title[1]
+			#print tstamp_str
+			tstamp_str = tstamp_str.replace(':',' ')
+			tstamp_str = tstamp_str.replace(',', ' ')
+			tstamp_str = tstamp_str.replace(' --> ', ' ')
+			tstamp_str = tstamp_str.split(' ')
+			tstamp = []
+			for s in tstamp_str:
+				tstamp.append(int(s))
+			stime = tstamp[0]*24 + tstamp[1]*60 + tstamp[2]
+			#print stime, tstamp
+	
+			if( stime < prev_stime ) :
+				logging.info("writing lines: {0}".format(line_no))
+				f.close()
+				# start new language subtitle
+				num_of_lang = num_of_lang + 1
+				wfname = self.srt_fname[0:-4] + '_' + str(num_of_lang) + '.srt'
+				logging.info("writing : {0}".format(wfname))
+				f = open(wfname, 'w')
+				line_no = 1
+				prev_stime = 0
+				stime = 0
+			else:
+				#st = subtitles[n]
+				title[0] = str(line_no)+'\n'
+				for s in title:
+					f.write(s.encode('utf-8'))
+					#print s,
+				f.write('\n'.encode('utf-8'))
+				line_no = line_no + 1
+			
+			prev_stime = stime
+			if( n >= len(subtitles)): # last line 
+				logging.info("writing lines: {0}".format(line_no))
+				f.close()
+				break
+			n = n + 1
+	
+	#--------------------------------------------------------------------------------------------------
+	def analysisSrt (self, lines, enc) :
+		logging.info("Analysis file ...")
+		subtitle = []
+		subtitles = []
+		n = 0
+		num_of_subtitles = 0
+		new_line_started = True
+		for line in lines:
+			line.lstrip()
+			line = line.replace('\r\n','\n')
+			line = line.decode(enc)
+			#print len(line), line,
+			if len(line) > 1 :
+				new_line_started = True
+			elif len(line) == 1:
+				new_line_started = False
+				subtitles.append(subtitle)
+				num_of_subtitles = num_of_subtitles +1
+				#print num_of_subtitles, subtitle[0]
+				subtitle = []
+			if new_line_started :
+				subtitle.append(line)
+			n = n+1
+		#	if num_of_subtitles > 20:
+		#		break
+		logging.info("{0} subtitles".format(len(subtitles)))
+		return subtitles
+	#--------------------------------------------------------------------------------------------------	
+	def doSeparateSrt(self):
+		logging.info("Start: {0}".format(self.srt_fname))
+		f = open(self.srt_fname, 'r')
+		lines = f.readlines()
+		count = len(lines)
+		logging.info("File: {0}".format(self.srt_fname))
+		logging.info("read {0} lines ".format(count))
+	
+		subtitles = self.analysisSrt(lines, self.encode)
+		logging.info("Writing file: {0}".format(self.srt_fname))
+		self.writeNewSrtFiles(subtitles)
+	#--------------------------------------------------------------------------------------------------
 
 
+###################################################################################################
+def usage(msg=None, exit_code=1):
+	print_msg = """
+	usage %s encoding
+	convert current directory srt file which have multiple langes data to individual srt files
+	By steven <ramsessk@gmail.com>
+""" % os.path.basename(sys.argv[0])
+	if msg:
+		print_msg += '%s\n' % msg
+	print print_msg
+	sys.exit(exit_code)
+
+###################################################################################################
+def main():
+#	if len(sys.argv) <= 1:
+#		usage()
+#	print "Converting SRTs to SRT. All srt files in current directory will be converted"
+#	doSeparateSrt(sys.argv[1])
+
+	logging.basicConfig(level=logging.INFO)
+	ex1 = SeparateSrtFiles(fname='../../tmp/kkk.srt')
+	ex1.doSeparateSrt()
+
+###################################################################################################
+if __name__ == '__main__':
+	main()
+	
+	
+
+'''
 def FindSrtFiles():
 	filenames = []
 	dirs = os.listdir('./')
@@ -185,26 +348,5 @@ def doSeparateSrt(enc):
 		print "Writing files ..."
 		#CopyBackup(src)
 		WriteNewSrtFiles(dest, subtitles)
-
-###################################################################################################
-def usage(msg=None, exit_code=1):
-	print_msg = """
-	usage %s encoding
-	convert current directory srt file which have multiple langes data to individual srt files
-	By steven <ramsessk@gmail.com>
-""" % os.path.basename(sys.argv[0])
-	if msg:
-		print_msg += '%s\n' % msg
-	print print_msg
-	sys.exit(exit_code)
-
-###################################################################################################
-def main():
-	if len(sys.argv) <= 1:
-		usage()
-	print "Converting SRTs to SRT. All srt files in current directory will be converted"
-	doSeparateSrt(sys.argv[1])
-
-###################################################################################################
-if __name__ == '__main__':
-	main()
+'''
+	
