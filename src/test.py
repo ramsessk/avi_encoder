1f0153fe8ae0e27e68a7f1205cbd3ae3fc0e562b
steven <ramsessk@gmail.com>
2014-02-15 14:43:35 +0800

    added test.py

--- src/test.py
+++ src/test.py
@@ -1,31 +1,61 @@
-import os, sys
-from stat import *
+#import os, sys
+#from stat import *
+#
+#def walktree(top, callback):
+#	'''recursively descend the directory tree rooted at top,
+#	   calling the callback function for each regular file'''
+#	
+#	for f in os.listdir(top):
+#		pathname = os.path.join(top, f)
+#		try:
+#			mode = os.stat(pathname).st_mode
+#		except:
+#			print "Error"
+#			continue
+#		
+#		if S_ISDIR(mode):
+#			# It's a directory, recurse into it
+#			walktree(pathname, callback)
+#		elif S_ISREG(mode):
+#			# It's a file, call the callback function
+#			callback(pathname)
+#		else:
+#			# Unknown file type, print a message
+#			print 'Skipping %s' % pathname
+#
+#def visitfile(filename):
+#	print 'visiting', filename
+#
+#
+#
+#if __name__ == '__main__':
+#	walktree(sys.argv[1], visitfile)
 
-def walktree(top, callback):
-	'''recursively descend the directory tree rooted at top,
-	   calling the callback function for each regular file'''
-	
-	for f in os.listdir(top):
-		pathname = os.path.join(top, f)
-		try:
-			mode = os.stat(pathname).st_mode
-		except:
-			print "Error"
-			continue
+
+
+import unittest
+import datetime
+
+class DatePattern:
+	def __init__(self, year, month, day):
+		self.date = datetime.date(year, month, day)
+	def matches(self, date):
+		return self.date == date
+
+class FooTests(unittest.TestCase):
+	def testMatches(self):
+		p = DatePattern(2004,9,28)
+		d = datetime.date(2004,9,28)
+		self.failUnless(p.matches(d))
 		
-		if S_ISDIR(mode):
-			# It's a directory, recurse into it
-			walktree(pathname, callback)
-		elif S_ISREG(mode):
-			# It's a file, call the callback function
-			callback(pathname)
-		else:
-			# Unknown file type, print a message
-			print 'Skipping %s' % pathname
+	def testMatchesFalse(self):
+		p = DatePattern(2004,9,28)
+		d = datetime.date(2004,9,29)
+		self.failIf(p.matches(d))
+		
 
-def visitfile(filename):
-	print 'visiting', filename
+def main():
+	unittest.main()
 
 if __name__ == '__main__':
-	walktree(sys.argv[1], visitfile)
-
+    main()
